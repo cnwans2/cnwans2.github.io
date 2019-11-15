dd7da5ab9be2710a9a6a211077fa39a0a41a7244
Cnwans2 <crystal.nwansi26@gmail.com>
2019-11-14 19:04:27 -0600

    hmk4

---
+++ Homework4/assests/logic.js
@@ -0,0 +1,72 @@
+var currentQuestion = 0
+var time = questions.length * 15
+var Timerid;
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+
+function startQuiz() {
+    var startScreen = document.getElementById("start-screen")
+    startScreen.setAttribute("class", "hide")
+
+    questionsEl.removeAttribute("class")
+    Timerid = setInterval(clockTick, 1000)
+
+    timerEl.textContent = time
+
+    getQuestions()
+
+}
+
+function getQuestions() {
+    var currentQuestions = questions[currentQuestion]
+
+
+    var title = document.getElementById("questions-title")
+    title.textContent = currentQuestions.title;
+    choicesEl.innerHTML = ""
+
+
+    currentQuestions.choices.forEach(function (choice, i) {
+        var choiceOptions = document.createElement("button")
+        choiceOptions.setAttribute("class", "choice")
+        choiceOptions.setAttribute("value", choice)
+        choiceOptions.textContent = i + 1 + "." + choice
+
+        choiceOptions.onclick = questionClick
+
+        choicesEl.appendChild(choiceOptions)
+    })
+}
+
+function questionClick() {
+    if (this.value !== questions[currentQuestion].answer) {
+        time -= 15
+        if (time<0) {
+            time=0
+        }
+        
+    timerEl.textContent = time 
+
+
+    }
+
+    currentQuestion++ 
+
+
+    
+}
+
+
+//fuction clock ticker
+//fuction save high score 
+//function to end quiz
+
+
+
---
+++ Homework4/assests/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "What is the least travled country?",
+      choices: ["Haiti", "Canada", "Tuvalu"],
+      answer: "Tuvalu"
+    },
+    {
+      title: "What languge is spoken in Togo",
+      choices: ["English", "French", "Togolese"],
+      answer: "French"
+    },
+     {
+        title: "What is the biggest country?",
+        choices: ["Russia", "Nigeria", "Australia"],
+        answer: "Russia"
+      },
+      {
+        title: "What is the smallest Country?",
+        choices: ["St. Marteen", "Tuvalu", "Vatican City"],
+        answer: "Vatican City"
+      },
+      {
+        title: "What is the most visted country?",
+        choices: ["USA", "France", "Jamacia"],
+        answer: "France"
+  
+      },
+    ]
